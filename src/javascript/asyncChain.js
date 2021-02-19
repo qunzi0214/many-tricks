bc18242601362ceea4a9d8abc8ef2e11721f627f
qunzi0214 <qunzi0214@gmail.com>
2021-02-19 15:29:20 +0800

    feat(javascript): add asyncChain

---
+++ src/javascript/asyncChain.js
@@ -0,0 +1,23 @@
+const asyncChain = fns => {
+  let index = 0
+  const last = fns[fns.length - 1]
+  const next = () => {
+    const fn = fns[index++]
+    fn === last ? fn() : fn(next)
+  }
+  next()
+}
+
+asyncChain([
+  next => {
+    console.log('0 seconds')
+    setTimeout(next, 1000)
+  },
+  next => {
+    console.log('1 second')
+    setTimeout(next, 1000)
+  },
+  () => {
+    console.log('2 second')
+  },
+])
