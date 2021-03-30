3b5b966b9718e1078a8fed8258bd1bac13901798
qunzi0214 <qunzi0214@gmail.com>
2021-03-30 10:28:42 +0800

    feat(javascript): add elementIsVisibleInViewport

---
+++ src/javascript/elementIsVisibleInViewport.js
@@ -0,0 +1,10 @@
+// 判断一个元素是否在视窗内，应用场景：曝光埋点
+// eslint-disable-next-line no-unused-vars
+const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
+  const { top, left, bottom, right } = el.getBoundingClientRect()
+  const { innerHeight, innerWidth } = window
+  return partiallyVisible
+    ? ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) &&
+        ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+    : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth
+}
