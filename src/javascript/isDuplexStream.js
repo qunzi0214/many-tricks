90dc8b995edf210dd5314ce67818227e3d630139
qunzi0214 <qunzi0214@gmail.com>
2021-07-13 14:48:21 +0800

    feat(javascript): add isDuplexStream

---
+++ src/javascript/isDuplexStream.js
@@ -0,0 +1,11 @@
+const isDuplexStream = val =>
+  val !== null &&
+  typeof val === 'object' &&
+  typeof val.pipe === 'function' &&
+  typeof val._read === 'function' &&
+  typeof val._readableState === 'object' &&
+  typeof val._write === 'function' &&
+  typeof val._writableState === 'object'
+
+const Stream = require('stream')
+isDuplexStream(new Stream.Duplex()) // true
