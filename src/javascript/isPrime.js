5fac6e06b2bd10b3890521514b17b6a9f712b613
qunzi0214 <qunzi0214@gmail.com>
2021-07-13 15:52:11 +0800

    feat(javascript): add isPrime

---
+++ src/javascript/isPrime.js
@@ -0,0 +1,7 @@
+const isPrime = num => {
+  const boundary = Math.floor(Math.sqrt(num))
+  for (let i = 2; i <= boundary; i++) if (num % i === 0) return false
+  return num >= 2
+}
+
+isPrime(11) // true
